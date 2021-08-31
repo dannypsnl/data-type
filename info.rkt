1929a0c7c3ccc765f3492f04d5726a18d7a6efaa
Lîm Tsú-thuàn≡悃悃≡林子篆 <dannypsnl@gmail.com>
2021-09-01 01:19:04 +0800

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "data-type")
-(define deps '("base"))
+(define deps '("base" "typed-racket-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/data-type.scrbl" ())))
 (define pkg-desc "Description Here")
