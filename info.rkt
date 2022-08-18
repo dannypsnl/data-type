1091a69fac483a61eb53f9936cd15eb76162b1d6
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-08-18 17:00:08 +0800

    dep: rackunit-typed
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- info.rkt
+++ info.rkt
@@ -1,7 +1,10 @@
 #lang info
 (define collection "data-type")
 (define deps '("base" "typed-racket-lib"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib"
+                     "racket-doc"
+                     "rackunit-lib"
+                     "rackunit-typed"))
 (define scribblings '(("scribblings/data-type.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
